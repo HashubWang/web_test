0b28259e9d75eeb33c581898bcdf947ab6d1dbc8
Hashub <wang.hashub@gmail.com>
2017-10-16 20:55:23 +0800

    js_commit

---
+++ js_learn.js
@@ -0,0 +1,10 @@
+var x = 13;
+var result = x/0;
+if (result == Infinity) {
+    console.log("result is infinity");
+}
+else{
+    console.log("result is not infinity");
+}
+var num = 0/0;
+console.log(num);
