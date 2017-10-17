357817cbe40f1e1de2ffde44cc4233fd71eb1f78
Hashub <wang.hashub@gmail.com>
2017-10-17 20:57:50 +0800

    for_loop

--- js_learn.js
+++ js_learn.js
@@ -7,4 +7,20 @@ else{
     console.log("result is not infinity");
 }
 var num = 0/0;
+
 console.log(num);
+
+var x = 0;
+var i;
+for (i=1; i<=10; i++) {
+    x = x + i;
+    console.log(x);
+}
+console.log(x);
+console.log("-----------------------")
+var t = 0;
+for (var i = 0; i <= 10; i++) {
+    t = t + 1;
+    console.log(t);
+}
+console.log(t);
